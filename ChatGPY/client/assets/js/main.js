985e9d6f2fd504d0d2fab0ad4212b026ea15cef2
Maxrom <maxrom6969@gmail.com>
2023-10-13 01:03:39 -0300

    Update_2 main.js

--- ChatGPY/client/assets/js/main.js
+++ ChatGPY/client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chatgpt-thga.onrender.com', {
+  const response = await fetch('https://www.mgrhosting.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
